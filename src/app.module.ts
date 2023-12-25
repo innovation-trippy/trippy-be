1ae40ef8b98ef2aa073216101adf5dc9e85762ee
Hyunho Son <aa000098@naver.com>
2023-12-25 14:12:13 +0900

    feat: upload source added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AvsecModule } from './avsec/avsec.module';
+import { ImageModule } from './image/image.module';
 
 @Module({
-  imports: [AvsecModule],
+  imports: [AvsecModule, ImageModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/image/image.controller.ts
@@ -0,0 +1,35 @@
+import { Body, Controller, FileTypeValidator, Get, ParseFilePipe, Post, UploadedFile, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import { ImageService } from './image.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+@Controller('image')
+export class ImageController {
+  constructor(private readonly imageService: ImageService) {}
+
+  @Get()
+  getForbid() {
+    return this.imageService.getForbid();
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('files'))
+  uploadFile(@UploadedFiles() files: Array<Express.Multer.File>) {
+    console.log(files);
+  }
+
+  @Post('uploadAndValidate')
+  uploadAndValidate(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new FileTypeValidator({ fileType: 'image/jpeg'}),
+        ],
+      }),
+    )
+    file: Express.Multer.File
+  ) {
+    return {
+      file: file.filename,
+    }
+  }
+}
