22ac0f13ee71470c4b944b07bbeff163b2cef9f2
Hyunho Son <aa000098@naver.com>
2023-12-28 12:55:18 +0900

    fix: AI 서버와 연결 완료

--- src/image/image.service.ts
+++ src/image/image.service.ts
@@ -11,30 +11,15 @@ export class ImageService {
         return "Hello World!!";
     }
 
-    async test() {
-        try {
-            let filepath = `/${join(PUBLIC_IMAGE_PATH, 'user_image.jpeg')}`;
-            const formData = new FormData();
-            formData.append('filepath', filepath);
-            const response = await this.httpService.axiosRef.post(
-                'https://trippy-ai-be-dreqf.run.goorm.site/upload',
-                { filepath: filepath },
-            );
-            return response.data;
-        } catch (error) {
-            console.error('Error:', error);
-            throw error;
-        }
-      }
-
     async detectObjectFromFile(file: Express.Multer.File) {
         try {
-            let filepath = `/${join(PUBLIC_IMAGE_PATH, file.filename)}`;
-            const formData = new FormData();
-            formData.append('filepath', filepath);
+            let filepath = `${join(PUBLIC_IMAGE_PATH, file.filename)}`;
+            const params = { filepath };
+
             const response = await this.httpService.axiosRef.post(
-                'https://trippy-ai-be-dreqf.run.goorm.site/uploadimg',
-                { filepath: filepath },
+                'https://trippy-ai-be-dreqf.run.goorm.site/upload',
+                null,
+                { params }
             );
             return response.data;
         } catch (error) {
