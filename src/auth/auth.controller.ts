186b044b022ac2777f3eb755dd0baab38cf15ed0
Hyunho Son <aa000098@naver.com>
2024-02-26 22:24:17 +0900

    fix: fix after kakao login to redirect trippy.kr

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -33,10 +33,7 @@ export class AuthController {
     res.cookie('kakaoToken', req.user.kakaoToken, { httpOnly: true });
     res.cookie('accessToken', accessToken, { httpOnly: true });
     res.cookie('refreshToken', refreshToken, { httpOnly: true });
-
-    return res.send({
-      message: 'success'
-    });
+    res.redirect('http://trippy.kr/check');
   }
 
   // 회원가입
